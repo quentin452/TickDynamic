88670b5fc5b5c6198b512b3d8e5a399bf7b6e7d6
Meldexun <27779321+Meldexun@users.noreply.github.com>
2023-08-17 19:16:00 +0200

    Remove TransformerPatcher from used class transformers

--- src/main/java/com/wildex999/tickdynamic/LoadingPlugin.java
+++ src/main/java/com/wildex999/tickdynamic/LoadingPlugin.java
@@ -1,8 +1,6 @@
 package com.wildex999.tickdynamic;
 
 import com.wildex999.patcher.EntityInjector;
-import com.wildex999.patcher.TransformerPatcher;
-import net.minecraft.launchwrapper.Launch;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin.SortingIndex;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin.TransformerExclusions;
@@ -16,13 +14,7 @@ public class LoadingPlugin implements IFMLLoadingPlugin {
 
 	@Override
 	public String[] getASMTransformerClass() {
-		boolean developmentEnvironment = (Boolean) Launch.blackboard.get("fml.deobfuscatedEnvironment");
-
-		//Only return the patcher if running in live environment, to allow for testing when running from eclipse/idea
-		if (!developmentEnvironment)
-			return new String[]{TransformerPatcher.class.getName(), EntityInjector.class.getName()};
-		else
-			return new String[]{EntityInjector.class.getName()};
+		return new String[]{EntityInjector.class.getName()};
 	}
 
 	@Override
